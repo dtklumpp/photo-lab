370ac1cfaa106e6683ecfed467dde5f95a9d7462
dtklumpp <dtklumpp@gmail.com>
2020-09-15 23:52:37 -0400

    made create route

--- controllers/photo_controller.js
+++ controllers/photo_controller.js
@@ -6,9 +6,18 @@ const db = require('../models/index.js');
 
 //index
 router.get('/', (req, res) => {
-    res.send('here be photos index page');
+    res.render('index.ejs');
 });
 
+//new route
 router.get('/newPhoto', (req, res) => {
     res.render('new.ejs');
+})
+
+//create route
+router.post('/', (req, res) => {
+    db.PhotoMod.create(req.body, (err, dat) => {
+        if(err) return console.log('err:', err);
+        res.redirect('/photos');
+    })
 })
